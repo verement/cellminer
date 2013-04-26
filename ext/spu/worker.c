2faa67a684c4e224c90b34d2b225d75931acdce7
Dennis Schridde <devurandom@gmx.net>
2013-04-26 12:40:58 +0200

    SPU: Instead of just verifying the midstate, use it

--- ext/spu/worker.c
+++ ext/spu/worker.c
@@ -25,20 +25,6 @@
 # include "util.h"
 
 /*
- * NAME:	verify_midstate()
- * DESCRIPTION:	check that the midstate received is correct
- */
-static
-int verify_midstate(const hash_t midstate, const message_t data)
-{
-  hash_t hash;
-
-  hash = sha256_update(data, H0);
-
-  return memcmp(&hash, &midstate, sizeof(hash_t)) == 0;
-}
-
-/*
  * NAME:	work_on()
  * DESCRIPTION:	search for a solution and return 1 if found, 0 otherwise
  */
@@ -47,24 +33,21 @@ int work_on(struct worker_params *params)
   int64_t nonce;
   char buf[257];
 
+  /* calculate the midstate of the received data */
+  hash_t midstate = sha256_update(params->data.m[0], H0);
+
   hex(buf, params->data.c, 128);
   debug("data     = %s", buf);
 
   debug_hash(&params->target.h,   "target  ");
-  debug_hash(&params->midstate.h, "midstate");
-
-  if (!verify_midstate(params->midstate.h,
-		       params->data.m[0])) {
-    debug("midstate verification failed");
-    return -1;
-  }
+  debug_hash(&midstate,           "midstate");
 
   debug("start_nonce = %08lx, range = %08lx",
 	params->start_nonce, params->range);
 
   nonce = sha256_search(params->data.m[1],
 			params->target.h,
-			params->midstate.h,
+			midstate,
 			params->start_nonce,
 			params->range);
   if (nonce < 0)
